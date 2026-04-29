0a179062bbb10feae8a12b770e9127290a1459d2
ShwetaKarl <karlewadshweta@gmail.com>
2026-04-30 01:06:20 +0530

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -13,19 +13,15 @@ from service.common import log_handlers
 app = Flask(__name__)
 app.config.from_object(config)
 
-# Import the routes After the Flask app is created
-# pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
 
-# pylint: disable=wrong-import-position
 from service.common import error_handlers, cli_commands  # noqa: F401 E402
 
-# Set up logging for production
 log_handlers.init_logging(app, "gunicorn.error")
 
 app.logger.info(70 * "*")
 app.logger.info(
-    "  A C C O U N T   S E R V I C E   R U N N I N G  ".center(70, "*")
+    "ACCOUNT SERVICE RUNNING".center(70, "*")
 )
 app.logger.info(70 * "*")
 
